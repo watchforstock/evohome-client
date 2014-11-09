04494f6c7485811e9c85a8c728e518d6f2f795f5
Andrew Blake <github@blakenet.uk>
2014-11-09 14:51:44 +0000

    Make debug configurable

--- evohomeclient2/__init__.py
+++ evohomeclient2/__init__.py
@@ -4,23 +4,14 @@ import json
 import codecs
 
 import logging
+logging.basicConfig()
+requests_log = logging.getLogger("requests.packages.urllib3")
 
-# These two lines enable debugging at httplib level (requests->urllib3->http.client)
-# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
-# The only thing missing will be the response.body which is not logged.
 try:
     import http.client as http_client
 except ImportError:
     # Python 2
     import httplib as http_client
-http_client.HTTPConnection.debuglevel = 1
-
-# You must initialize logging, otherwise you'll not see debug output.
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
 
 
 class EvohomeClientInvalidPostData(Exception):
@@ -28,7 +19,19 @@ class EvohomeClientInvalidPostData(Exception):
 
 
 class EvohomeClient:
-    def __init__(self, username, password):
+    def __init__(self, username, password, debug=False):
+
+        if debug:
+            http_client.HTTPConnection.debuglevel = 1
+            logging.getLogger().setLevel(logging.DEBUG)
+            requests_log.setLevel(logging.DEBUG)
+            requests_log.propagate = True
+        else:
+            http_client.HTTPConnection.debuglevel = 0
+            logging.getLogger().setLevel(logging.INFO)
+            requests_log.setLevel(logging.INFO)
+            requests_log.propagate = False
+
         self.username = username
         self.password = password
         self.reader = codecs.getdecoder("utf-8")
