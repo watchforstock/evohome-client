838c2789571dd0351667ae1c24cc9c9ee321a750
luke west <luke@novadrine.com>
2020-10-07 08:32:20 +0100

    add ability to get DHW status in evohomeclient2

--- evohomeclient2/hotwater.py
+++ evohomeclient2/hotwater.py
@@ -1,4 +1,4 @@
-"""Provide handling of the hot water zone."""
+"""Provides handling of the hot water zone."""
 import json
 
 import requests
@@ -7,25 +7,22 @@ from .zone import ZoneBase
 
 
 class HotWater(ZoneBase):
-    """Provide handling of the hot water zone."""
+    """Provides handling of the hot water zone."""
 
     def __init__(self, client, data):
-        """Initialise the class."""
         super(HotWater, self).__init__(client)
 
-        self.dhwId = None  # pylint: disable=invalid-name
+        self.dhwId = None                                                        # pylint: disable=invalid-name
 
         self.__dict__.update(data)
 
         self.name = ""
         self.zoneId = self.dhwId
-        self.zone_type = "domesticHotWater"
+        self.zone_type = 'domesticHotWater'
 
     def _set_dhw(self, data):
-        # pylint: disable=protected-access
-        headers = dict(self.client._headers())
-        headers["Content-Type"] = "application/json"
-
+        headers = dict(self.client._headers())                                   # pylint: disable=protected-access
+        headers['Content-Type'] = 'application/json'
         url = (
             "https://tccna.honeywell.com/WebAPI/emea/api/v1"
             "/domesticHotWater/%s/state" % self.dhwId
@@ -35,33 +32,47 @@ class HotWater(ZoneBase):
         response.raise_for_status()
 
     def set_dhw_on(self, until=None):
-        """Set the DHW on until a given time, or permanently."""
+        """Sets the DHW on until a given time, or permanently."""
         if until is None:
-            data = {"Mode": "PermanentOverride", "State": "On", "UntilTime": None}
+            data = {"Mode": "PermanentOverride",
+                    "State": "On",
+                    "UntilTime": None}
         else:
-            data = {
-                "Mode": "TemporaryOverride",
-                "State": "On",
-                "UntilTime": until.strftime("%Y-%m-%dT%H:%M:%SZ"),
-            }
+            data = {"Mode": "TemporaryOverride",
+                    "State": "On",
+                    "UntilTime": until.strftime('%Y-%m-%dT%H:%M:%SZ')}
 
         self._set_dhw(data)
 
     def set_dhw_off(self, until=None):
-        """Set the DHW off until a given time, or permanently."""
+        """Sets the DHW off until a given time, or permanently."""
         if until is None:
-            data = {"Mode": "PermanentOverride", "State": "Off", "UntilTime": None}
+            data = {"Mode": "PermanentOverride",
+                    "State": "Off",
+                    "UntilTime": None}
         else:
-            data = {
-                "Mode": "TemporaryOverride",
-                "State": "Off",
-                "UntilTime": until.strftime("%Y-%m-%dT%H:%M:%SZ"),
-            }
+            data = {"Mode": "TemporaryOverride",
+                    "State": "Off",
+                    "UntilTime": until.strftime('%Y-%m-%dT%H:%M:%SZ')}
 
         self._set_dhw(data)
 
     def set_dhw_auto(self):
-        """Set the DHW to follow the schedule."""
-        data = {"Mode": "FollowSchedule", "State": "", "UntilTime": None}
+        """Sets the DHW to follow the schedule."""
+        data = {"Mode": "FollowSchedule",
+                "State": "",
+                "UntilTime": None}
 
         self._set_dhw(data)
+
+
+    def get_dhw(self):
+        """Gets the DHW status."""
+        url = (
+            "https://tccna.honeywell.com/WebAPI/emea/api/v1/"
+            "domesticHotWater/%s/status?" % self.dhwId
+        )
+
+        response = requests.get( url, headers=self.client._headers())
+        data = response.json()
+        return data
