8db3538c5e2b7b61b6367992b7ee2afc7fc72ef7
Sean Holtshousen <holtshousens@gmail.com>
2020-11-13 15:44:41 +0000

    adding unending loop

--- evohome_client_implementation.py
+++ evohome_client_implementation.py
@@ -1,6 +1,7 @@
 from evohomeclient2 import EvohomeClient;
 import pyodbc;
 from datetime import datetime;
+import time;
 import configparser;
 
 config = configparser.ConfigParser()
@@ -15,9 +16,6 @@ username = config.get('evohome', 'username');
 password = config.get('evohome', 'password');
 
 CONNECTION_STRING = 'DRIVER=' + driver + '; SERVER=' + server + '; DATABASE=' + databasename + '; Trusted_Connection=yes';
-connection = pyodbc.connect(CONNECTION_STRING);
-
-client = EvohomeClient(username, password, debug=True)
 
 def insert_zones(thermostat, id, name, temp, setpoint):
     cursor = connection.cursor();
@@ -26,17 +24,31 @@ def insert_zones(thermostat, id, name, temp, setpoint):
         dbo.Zones (uid, timestamp, thermostat, id, [name], temp, setpoint) 
         VALUES (NEWID(), CURRENT_TIMESTAMP, \'''' + thermostat + '''\', \'''' + id + '''\', \'''' + name + '''\', \'''' + str(temp) + '''\', \'''' + str(setpoint) + '''\')
     '''
-    
-    print(sSQL);
-    
+
     cursor.execute(sSQL);
     
     print('id: ' + id + ' inserted')
     
     cursor.commit();
 
-for device in client.temperatures():
-    print(device)
-    insert_zones(thermostat=device['thermostat'], id=device['id'], name=device['name'], temp=device['temp'], setpoint=device['setpoint'])
+# Infinite loop every 5 minutes, send temperatures to sql
+while True:
+    connection = pyodbc.connect(CONNECTION_STRING);
 
-connection.close();
+    try:
+        client = EvohomeClient(username, password, debug=True)
+    except ValueError:
+        try:
+            client = EvohomeClient(username, password, debug=True)
+        except ValueError:
+            print("Error when connecting to internet, please try again")
+
+
+    for device in client.temperatures():
+        print(device)
+        insert_zones(thermostat=device['thermostat'], id=device['id'], name=device['name'], temp=device['temp'], setpoint=device['setpoint'])
+
+    connection.close();
+
+    print ("Going to sleep for 5 minutes")
+    time.sleep(300)
