539c8e82624260982fe342ada6480bce093e8bf4
Sean Holtshousen <holtshousens@gmail.com>
2020-11-13 12:34:09 +0000

    adding datetimestamp and moving things to a config

--- evohome_client_implementation.py
+++ evohome_client_implementation.py
@@ -1,7 +1,42 @@
-from evohomeclient2 import EvohomeClient
+from evohomeclient2 import EvohomeClient;
+import pyodbc;
+from datetime import datetime;
+import configparser;
 
-# Add username and password here
-#client = EvohomeClient('username', 'password', debug=True)
+config = configparser.ConfigParser()
+
+config.read("config.ini")
+
+databasename = config.get('DB', 'dbname');
+server = config.get('DB', 'dbserver');
+driver = config.get('DB', 'dbdriver'); 
+
+username = config.get('evohome', 'username');
+password = config.get('evohome', 'password');
+
+CONNECTION_STRING = 'DRIVER=' + driver + '; SERVER=' + server + '; DATABASE=' + databasename + '; Trusted_Connection=yes';
+connection = pyodbc.connect(CONNECTION_STRING);
+
+client = EvohomeClient(username, password, debug=True)
+
+def insert_zones(thermostat, id, name, temp, setpoint):
+    cursor = connection.cursor();
+    sSQL = '''
+        INSERT INTO 
+        dbo.Zones (uid, timestamp, thermostat, id, [name], temp, setpoint) 
+        VALUES (NEWID(), CURRENT_TIMESTAMP, \'''' + thermostat + '''\', \'''' + id + '''\', \'''' + name + '''\', \'''' + str(temp) + '''\', \'''' + str(setpoint) + '''\')
+    '''
+    
+    print(sSQL);
+    
+    cursor.execute(sSQL);
+    
+    print('id: ' + id + ' inserted')
+    
+    cursor.commit();
 
 for device in client.temperatures():
     print(device)
+    insert_zones(thermostat=device['thermostat'], id=device['id'], name=device['name'], temp=device['temp'], setpoint=device['setpoint'])
+
+connection.close();
